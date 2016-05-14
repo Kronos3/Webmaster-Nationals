f366834fe4841bbe574a442f78754e2217aa4156
Kronos3 <dovakhiin1359@gmail.com>
2016-05-13 23:20:22 -0400

    Created scroll to change tab background tab color

---
+++ css/scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 500) {
+                    $("#back").css('background-color', '#585858');
+                }
+                else {
+                    $("#back").css('background-color', 'rgba(255,255,255,0)');
+                }
+            });
+        });
