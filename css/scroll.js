393bb1e0955ffc08c3708a89509185d3063a4769
Kronos3 <dovakhiin1359@gmail.com>
2016-05-15 12:15:53 -0400

    Made css and js level with other two sites

--- css/scroll.js
+++ css/scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var scroll_pos = 0;
     $(document).scroll(function() { 
         scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 200) {
+        if(scroll_pos > -1) {
             $("#back").css('background-color', '#585858');
         }
         else {
