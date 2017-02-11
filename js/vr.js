a1038612ca2acd124330e7750064f0bffd45a108
Kronos3 <dovakhiin1359@gmail.com>
2017-02-10 21:01:24 -0500

    Finished content styling

--- js/vr.js
+++ js/vr.js
@@ -7,3 +7,15 @@ String.prototype.format = String.prototype.f = function() {
     }
     return s;
 };
+
+$(function() {
+    $('.fadein img:gt(0)').hide();
+    
+    setInterval(function () {
+        $('.fadein :first-child').fadeOut()
+                                 .next('img')
+                                 .fadeIn()
+                                 .end()
+                                 .appendTo('.fadein');
+    }, 4000); // 4 seconds
+});
