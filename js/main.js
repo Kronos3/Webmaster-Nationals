b37323c0afc2bb1879d1e072677a8cb181b7f353
kronos3 <dovakhiin1359@gmail.com>
2017-04-03 00:23:04 -0400

    Added scroll steps Flush data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+function next (e) {
+    
+}
 
+function prev (e) {
+    
+}
+
+$( document ).ready(function() {
+    $("body").scrollsteps({
+        up: yourUpFunction,
+        down: yourUpFunction,
+        transitionDuration: 1000,
+        quietPeriodBetweenTwoScrollEvents: 200,
+    });
+}
