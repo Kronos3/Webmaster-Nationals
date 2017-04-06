5e2a5d0aa96d2ddd6263e050fee7758bacafb83a
Andrei Tumbar <dovakhiin1359@gmail.com>
2017-04-06 18:22:58 -0400

    Working timeline

--- js/main.js
+++ js/main.js
@@ -7,10 +7,24 @@ function prev (e) {
 }
 
 $( document ).ready(function() {
+    console.log($('.timeline > .__step').length);
+    for (var i=0; i != $('.timeline > .__step').length; i++) {
+        console.log ($('.timeline > .__step').get(i));
+        $('.timeline > .__step').get(i).addEventListener('click', function (e) {
+            var f = $(this).index();
+            $('.top').css('transform', 'translateY(' + (-100*f).toString() + '%)');
+            $('.__step.active').toggleClass ('active');
+            $($('.__step').get(f)).toggleClass('active');
+        });
+    }
+});
+
+
+$( document ).ready(function() {
     $("body").scrollsteps({
         up: prev,
         down: next,
         transitionDuration: 1000,
-        quietPeriodBetweenTwoScrollEvents: 200,
+        quietPeriodBetweenTwoScrollEvents: 400,
     });
 });
